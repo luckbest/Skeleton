c7d122e2f3f95e99d2d8f4ea03b9b4224b966aac
luckbest <przywara.luck@gmail.com>
2014-02-24 16:46:13 +0100

    Start - firts commit to skeleton: ZFTools +doctrine orm

---
+++ module/Application/src/Application/Entity/Test.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/** @ORM\Entity */
+class Test {
+    /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    * @ORM\Column(type="integer")
+    */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $fullName;
+
+    // getters/setters
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fullName
+     *
+     * @param string $fullName
+     * @return Test
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * Get fullName
+     *
+     * @return string 
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+}
